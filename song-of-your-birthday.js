92a0119ee95a3937b60b8efd3ec89c63b7cbbf39
Michael Benford <michael@michaelbenford.net>
2017-03-29 20:45:15 -0300

    Update song-of-your-birthday.js

--- song-of-your-birthday.js
+++ song-of-your-birthday.js
@@ -11,12 +11,10 @@ var view = (function view() {/*
     <body>
       <div class="container">
         <h1 class="text-center">Song of your birthday</h1>
-        <% if (unavailable) { %>
-        <p class="lead text-center">Ooops... According to the Billboard Hot 100, there is no song available for the week you were born. What a bummer! :(</p>
-        <% } else { %>
+        <% if (title) { %>
         <p class="lead text-center">
           According to the Billboard Hot 100, the most popular song <a href="http://www.billboard.com/charts/hot-100/<%= date %>">in the week you were born</a> is
-          <br><strong><%= bmdb_title %></strong> by <strong><%= bmdb_artistname %></strong>.
+          <br><strong><%= title %></strong> by <strong><%= artist %></strong>.
         </p>
         <p class="lead text-center">
           And here is the first search result for that song on YouTube (results may vary)
@@ -24,6 +22,8 @@ var view = (function view() {/*
         <div class="text-center">
           <iframe width="640" height="480" src="https://www.youtube.com/embed/<%= videoId %>">
         </div>
+        <% } else { %>
+        <p class="lead text-center">Ooops... According to the Billboard Hot 100, there is no song available for the week you were born. What a bummer! :(</p>
         <% } %>
       </div>
     </body>
@@ -41,6 +41,10 @@ module.exports = function(context, req, res) {
   getTrackForDate(date).then(getYouTubeVideo).then(function(track) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(require('ejs').render(view, track));
+  })
+  .catch(function(error) {
+    res.writeHead(500, { 'Content-Type': 'text/html' });
+    res.end('That\'s embarassing, but something went wrong: ' + error);
   });
 };
 
@@ -50,13 +54,17 @@ function getTrackForDate(date) {
       json: true
     };
     return request(options).then(function(result) {
-      var track = result === [] ? null : result.rows[0];
-      return track;
+      var row = result && result.rows ? result.rows[0] : {};
+      return {
+        title: row.bmdb_title,
+        artist: row.bmdb_artistname,
+        date: row.date
+      };
     });
 }
 
 function getYouTubeVideo(track) {
-  if (track === null) return Promise.resolve({ unavailable: true });
+  if (!track.title) return Promise.resolve(track);
   
   var query = track.title + ' ' + track.artist;
   var url = 'https://www.youtube.com/results?search_query=' + query;
@@ -65,6 +73,7 @@ function getYouTubeVideo(track) {
     var href = $('.yt-lockup-title a').attr('href'); 
     var videoId = /v=(.+)/.exec(href);
     track.videoId = videoId ? videoId[1] : '';
+    track.unavailable = false;
     return track;
   });
 }
